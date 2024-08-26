65c7a16d3bbc81bfb6476f4fcaf4ca62778c6b00
Jack-M1 <jack.mcgr0@gmail.com>
2024-08-26 15:23:57 +0100

    More docker work

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ RUN mkdir -p /code
 WORKDIR /code
 COPY . /code
 
+ARG API_URL
+ENV API_URL=${API_URL}
+
 RUN npm install
 RUN npm run build
 
@@ -11,4 +14,4 @@ EXPOSE 3000
 ENV NUXT_HOST=0.0.0.0
 ENV NUXT_PORT=3000
 
-CMD ["npm". "start"]
+CMD [ "npm", "start" ]
--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
-        const response: AxiosResponse = await axios.get("http://localhost:8080/api/test");
+        const response: AxiosResponse = await axios.get("${API_URL}");
 
         return response.data;
     } catch (e) {
