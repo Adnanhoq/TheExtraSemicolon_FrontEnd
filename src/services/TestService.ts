4e1174b5dd7483ad0a49e8db750c9ae2a732045b
Jack-M1 <114680493+Jack-M1@users.noreply.github.com>
2024-08-27 17:01:25 +0100

    007b deployfrontend (#13)
    * Added app.get getAllDatabases
    
    * Pushing for branch
    
    * Branch
    
    * work
    
    * commit new url
    
    ---------
    
    Co-authored-by: jemorak <jemima.orakwue@hotmail.co.uk>

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -3,8 +3,8 @@ import config from "../config";
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
-          console.log(config.API_URL);
-        const response: AxiosResponse = await axios.get(config.API_URL);
+        console.log(config.API_URL);
+        const response: AxiosResponse = await axios.get(config.API_URL+"test");
 
         return response.data;
     } catch (e) {
