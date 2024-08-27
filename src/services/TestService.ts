f45c73ca27a8d42c0e17766a8e21202de86cd24f
Jack-M1 <jack.mcgr0@gmail.com>
2024-08-27 15:34:10 +0100

    Pushing for branch

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -3,11 +3,14 @@ import config from "../config";
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
-          console.log(config.API_URL);
-        const response: AxiosResponse = await axios.get(config.API_URL);
+        console.log("shoul;d be here");
+        console.log(config.API_URL);
+        const response: AxiosResponse = await axios.get("https://5tmqdqfjni.eu-west-1.awsapprunner.com/api/test");
 
         return response.data;
     } catch (e) {
+        console.log("should be here")
+        console.log(config.API_URL)
         console.log(e);
         throw new Error('Failed to get databases');
     }
