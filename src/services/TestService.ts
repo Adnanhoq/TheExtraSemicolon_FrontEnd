dec9328d5b09ce870c46f5f79dabda38cfd61ac7
jemorak <jemima.orakwue@hotmail.co.uk>
2024-08-27 11:52:21 +0100

    updated deploy frontend

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosResponse } from "axios";
-import { API_URL } from "../config";
-import 'dotenv/config';
+import config from "../config";
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
-          console.log(API_URL);
-        const response: AxiosResponse = await axios.get(API_URL);
+          console.log(config.API_URL);
+        const response: AxiosResponse = await axios.get(config.API_URL);
 
         return response.data;
     } catch (e) {
