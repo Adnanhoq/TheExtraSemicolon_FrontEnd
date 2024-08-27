b22c6af051900271e7780fc00179c869a4448ee6
Jack-M1 <jack.mcgr0@gmail.com>
2024-08-27 16:11:52 +0100

    work

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -5,12 +5,10 @@ export const getDatabases = async (): Promise<string[]> => {
     try {
         console.log("shoul;d be here");
         console.log(config.API_URL);
-        const response: AxiosResponse = await axios.get("https://5tmqdqfjni.eu-west-1.awsapprunner.com/api/test");
+        const response: AxiosResponse = await axios.get(config.API_URL);
 
         return response.data;
     } catch (e) {
-        console.log("should be here")
-        console.log(config.API_URL)
         console.log(e);
         throw new Error('Failed to get databases');
     }
