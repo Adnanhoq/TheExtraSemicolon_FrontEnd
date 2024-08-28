35f941f27a3e95a81780d4fc59263ff06f534a56
mohammed <mohammedadnan.hoq@kainos.com>
2024-08-28 15:09:39 +0100

    Fixed Unit Test

--- test/unit/services/JobRoleServiceTests.ts
+++ test/unit/services/JobRoleServiceTests.ts
@@ -5,13 +5,13 @@ import { JobRoleResponse } from "../../../src/models/JobRoleResponse";
 import { getJobRoles, URL } from '../../../src/services/JobRoleService';
 
 
-const jobRoleRequest: JobRoleResponse = {
+const jobRoleResponse: JobRoleResponse = {
     roleId: 1,
     roleName: "Technical Architect",
     locations: "Birmingham",
     capability: "Engineering",
     band: "Manager",
-    closingDate: new Date(1990, 4, 7).toISOString
+    closingDate: new Date(1580782960)
 }
 
 
@@ -20,13 +20,15 @@ const mock = new MockAdapter(axios);
 describe('JobRoleService', function () {
     describe('getAllJobRoles', function () {
       it('should return all Job Roles from response', async () => {
-        const data = [jobRoleRequest];
+        const data = [jobRoleResponse];
 
         mock.onGet(URL).reply(200, data);
 
         const results = await getJobRoles();
 
-        expect(results[0]).to.deep.equal(jobRoleRequest);
+        results[0].closingDate = new Date(results[0].closingDate);
+
+        expect(results[0]).to.deep.equal(jobRoleResponse);
       })
 
       it('should throw exception when 500 error returned from axios', async () => {
