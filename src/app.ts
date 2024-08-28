e3ffd29f61512eb8caa7e54862931e84d75d9d59
Jack-M1 <114680493+Jack-M1@users.noreply.github.com>
2024-08-28 15:23:37 +0100

    007 Adding linter checks (#8)
    * Added linter yml file for github actions and installed eslint in jsons
    
    * Added custom google ruleset for ESLint and changed linter yml file for installing dependencies
    
    * Linter checks work but detects errors in js files
    
    * more eslint work
    
    * Added further commands to eslint file to ensure it ignores certain directories
    
    * Delete .env
    
    * Delete .eslintignore
    
    * Trying to resolve GitHub build failures
    
    * More work on linter
    
    * Fixed config.ts linter issues
    
    * Fixed remaining linter issues
    
    ---------
    
    Co-authored-by: Sam Dagnall <sam.dagnall@kainos.com>

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,8 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.render('index.njk');
 });
 
-app.get('/test', getAllDatabases);
+app.get('/test', (() => getAllDatabases));
--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosResponse } from "axios";
+import axios, {AxiosResponse} from "axios";
 import config from "../config";
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
         console.log(config.API_URL);
-        const response: AxiosResponse = await axios.get(config.API_URL+"test");
+        const response: AxiosResponse<string[]> = await axios.get(config.API_URL+"test");
 
         return response.data;
     } catch (e) {
