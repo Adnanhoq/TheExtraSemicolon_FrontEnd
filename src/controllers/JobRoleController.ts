c13c2260d9d8e8baaf1ae3b12d70309034256f19
mohammed <mohammedadnan.hoq@kainos.com>
2024-08-28 14:41:19 +0100

    Front End tests (half done)

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -2,5 +2,5 @@ import express from "express";
 import { getJobRoles } from "../services/JobRoleService";
 
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('jobRoles.njk', {jobRole: await getJobRoles(req.session.token) });
+    res.render('jobRoles.njk', {jobRole: await getJobRoles() });
 }
--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -1,9 +1,13 @@
 import axios, { AxiosResponse } from "axios";
 import { JobRoleResponse } from "../models/JobRoleResponse";
 
-export const getJobRoles = async (token: String): Promise<JobRoleResponse[]> => {
+axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080/';
+
+export const URL: string = "/api/job-roles/";
+
+export const getJobRoles = async (): Promise<JobRoleResponse[]> => {
     try{
-        const response: AxiosResponse = await axios.get("http://localhost:8080/api/job-roles/");
+        const response: AxiosResponse = await axios.get(URL);
         return response.data;
     } catch (e) {
         console.log(e);
