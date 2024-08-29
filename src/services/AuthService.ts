2691a4c9d9f48e4ad153149d7cae7dfd9d156d9c
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-29 10:07:51 +0100

    Fixed unit test errors

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -9,15 +9,11 @@ export const getToken = async (loginRequest: LoginRequest): Promise<string> => {
         validateLogin(loginRequest.email, loginRequest.password);
         const hashedPwd: Buffer = pbkdf2Sync(loginRequest.password, "", 65536, 6*16, 'sha256');
         loginRequest.password = hashedPwd.toString('base64');
-        const response: AxiosResponse = await axios.post(config.API_URL+"//auth/login", loginRequest);
+        const response: AxiosResponse = await axios.post(config.API_URL+"auth/login", loginRequest);
 
         return response.data;
     } catch (e) {
         console.log(e);
-
-        if (!e.response) {
-            throw new Error('Something went wrong during log in');          
-        }
 
         if (e.response && axios.isAxiosError(e)) {
             if (e.response.status == 404 || e.response.status == 500) {
