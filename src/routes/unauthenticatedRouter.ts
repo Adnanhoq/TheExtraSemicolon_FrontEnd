2f492693e6116d616879ef061c8ddd001a0f7b28
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-30 11:36:06 +0100

    Fixed Linter and Build issues

--- src/routes/unauthenticatedRouter.ts
+++ src/routes/unauthenticatedRouter.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import { getIndexPage } from '../controllers/HomeController';
 import { getLoginForm, postLoginForm, getLogout } from '../controllers/AuthController';
 import { getAllDatabases } from '../controllers/TestController';
 
 export const unauthenticatedRouter = express.Router();
 
-unauthenticatedRouter.get('/', (() => getIndexPage));
-unauthenticatedRouter.get('/login', (() => getLoginForm));
-unauthenticatedRouter.post('/login', (() => postLoginForm));
-unauthenticatedRouter.get('/logout', (() => getLogout));
-unauthenticatedRouter.get('/test', (() => getAllDatabases));
+unauthenticatedRouter.get('/', getIndexPage);
+unauthenticatedRouter.get('/login', getLoginForm);
+unauthenticatedRouter.post('/login', (postLoginForm as RequestHandler));
+unauthenticatedRouter.get('/logout', getLogout);
+unauthenticatedRouter.get('/test', (getAllDatabases as RequestHandler));
