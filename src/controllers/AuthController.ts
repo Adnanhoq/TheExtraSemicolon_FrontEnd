60ea429d9f3823930d03046b2b104cd94c8f4fa8
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-30 08:47:20 +0100

    Resolved all Linter errors

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getToken } from "../services/AuthService";
+import { LoginRequest } from "../models/LoginRequest";
 
 export const getLoginForm = (req: express.Request, res: express.Response) => {
     res.render('loginForm.njk', { token: req.session.token});
@@ -7,24 +8,23 @@ export const getLoginForm = (req: express.Request, res: express.Response) => {
 
 export const postLoginForm = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        req.session.token = await getToken(req.body);
+        req.session.token = await getToken(req.body as LoginRequest);
         res.redirect('/');
     } catch (e) {
         res.locals.errormessage = (e as Error).message;
-        res.render('loginForm.njk', req.body);
+        res.render('loginForm.njk', req.body as LoginRequest);
     }
 }
 
 export const getLogout = (req: express.Request, res: express.Response) => {
-    if(req.session) {
-        req.session.destroy(err => {
-            if (err) {
-                res.locals.errormessage = "Unable to log out";
-                res.render('/login');
-            } else {
-                res.redirect('/');
-            }
-        });
-    }
+    req.session.destroy(err => {
+        if (err) {
+            res.locals.errormessage = "Unable to log out";
+            res.render('/login');
+        } else {
+            res.redirect('/');
+        }
+    });
+    
     
 } 
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosResponse} from "axios";
+import axios, { AxiosResponse } from "axios";
 import type { LoginRequest } from "../models/LoginRequest";
 import { pbkdf2Sync } from "crypto";
 import { validateLogin } from "../validators/LoginValidator";
@@ -10,16 +10,15 @@ export const getToken = async (loginRequest: LoginRequest): Promise<string> => {
         const hashedPwd: Buffer = pbkdf2Sync(loginRequest.password, "", 65536, 6*16, 'sha256');
         loginRequest.password = hashedPwd.toString('base64');
         const response: AxiosResponse = await axios.post(config.API_URL+"auth/login", loginRequest);
-        return response.data;
+        return response.data as string;
     } catch (e) {
         console.log(e);
-
-        if (e.response && axios.isAxiosError(e)) {
+        if (axios.isAxiosError(e) && e.response != undefined) {
             if (e.response.status == 404 || e.response.status == 500) {
                 throw new Error('Something went wrong during log in'); 
             }
 
-            throw new Error(e.response.data);
+            throw new Error(e.response.data as string);
         }
         throw new Error((e as Error).message);
     }
