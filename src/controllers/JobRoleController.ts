f9e2ea7bff7fd1c88db123e132818470dff924cb
mohammed <mohammedadnan.hoq@kainos.com>
2024-08-30 10:29:49 +0100

    Removed logging

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -2,7 +2,5 @@ import express from "express";
 import { getJobRoles } from "../services/JobRoleService";
 
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
-    const jobRole = await getJobRoles();
-    console.log(jobRole)
     res.render('jobRoles.njk', {jobRole: await getJobRoles() });
 }
