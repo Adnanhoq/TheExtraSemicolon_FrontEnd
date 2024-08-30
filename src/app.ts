f02b2f7446e958541668e2e755e05bca257b30d1
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-30 13:39:57 +0100

    Changed token name to kainos-job-roles

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.static("views"));
 // Specifying folder from where to fetch images
 app.use('/assets', express.static('./assets')); 
 
-app.use(session({ secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 }}));
+app.use(session({ name:'kainos-job-roles', secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 }}));
 
 declare module "express-session" {
   interface SessionData {
