3432a72d561c779f0f699f9eb783263316a1f826
sdagn1 <77390325+sdagn1@users.noreply.github.com>
2024-09-02 16:24:06 +0100

    008 - Adding a Login system (#7)
    * Added functionality to log-in via the frontend.
    
    * Added logout button
    
    * Added logout button that redirects to login page
    
    * Added front-end validation to reduce the number of API calls to the database
    
    * Added functionality to display Log Out when signed in through the header
    
    * Linter fixes
    
    * Increased max length of email
    
    * Added front-end validation for the login system
    
    * Changed pbkdf2 to dev dependency
    
    * Made fixes according to Code Review
    
    * Configured the front-end to use a 'main' template
    
    * Added validation unit tests
    
    * Added routing for unauthenticated users
    
    * Adjusted error message if user cannot log out
    
    * Added type to LoginRequest import
    
    * Started to implement service/controller Unit Tests
    
    * Added final unit tests and accessibility changes
    
    * Added Unit Testing GitHub Workflow to Repo
    
    * Repositioned Unit test Workflow
    
    * Added dependency installation to github workflow
    
    * Condensed Validation tests to become parameterized
    
    * Condensed Service validation tests to be a parameterized test
    
    * Added name to unit test workflow
    
    * Added name to unit test workflow
    
    * Changed pathing to use API_URL instead of hardcoded value
    
    * Adjusted URL in tests to not be hardcoded
    
    * Refactored API_URL links
    
    * Added more catches for error logging
    
    * Fixed unit test errors
    
    * Started to fix Linter issues
    
    * Further Linter fixes
    
    * Resolved all Linter errors
    
    * Fixed Linter and Build issues
    
    * Changed linter fixes to be consistent
    
    * Removed unused RequestHandler
    
    * Changed token name to kainos-job-roles
    
    * Made Linter configuration to remove error
    
    * Added new error messages if User leaves email/password blank
    
    * Changed colour of error messages
    
    * Changed email form type to text
    
    * Fixed error messaging
    
    * Added to the ReadME with user credentials

---
+++ src/validators/LoginValidator.ts
@@ -0,0 +1,24 @@
+export const validateLogin = (email: string, password: string) => {
+
+    if (email.length == 0) {
+        throw new Error("No email entered");
+    }
+
+    if (password.length == 0) {
+        throw new Error("No password entered");
+    }
+
+    if (email.length > 255) {
+        throw new Error("Email is not valid length");
+    }
+
+    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    if (!regexp.test(email)) {
+        throw new Error("Email is not valid format");
+    }
+
+    if (password.length == 0 || password.length > 64) {
+        throw new Error("Password is not valid");
+    }
+}
---
+++ test/unit/controllers/AuthControllerTests.ts
@@ -0,0 +1,85 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import * as AuthService from "../../../src/services/AuthService"
+import * as AuthController from "../../../src/controllers/AuthController"
+import * as AxiosRequest from 'axios';
+
+declare module "express-session" {
+    interface SessionData {
+      token: string;
+    }
+  }
+
+describe('AuthController', function () {
+    this.afterEach(() => {
+        sinon.restore();
+    });
+
+    describe('getLoginForm', function () {
+        it('should render view with login form when retrieved', async () => {
+            //sinon.stub(AuthService, 'getToken').resolves('');
+            const req = { session: { token: 'test' }};
+            const res = { render: sinon.spy()};
+
+            await AuthController.getLoginForm(req as any, res as any);
+
+            expect(res.render.calledOnce).to.be.true;
+            expect(res.render.calledWith('loginForm.njk')).to.be.true;
+        });
+    }),
+    describe('postLoginForm', function () {
+        it('should redirect to home when login successful', async () => {
+            const token = '';
+            sinon.stub(AuthService, 'getToken').resolves(token);
+
+            const req = { session: {token: ''}};
+            const res = { render: sinon.spy(), redirect: sinon.spy() };
+
+            await AuthController.postLoginForm(req as any, res as any);
+            expect(res.redirect.calledOnce).to.be.true;        
+            expect(res.redirect.calledWith('/')).to.be.true;
+        }),
+        it('should render view with error message when error thrown', async () => {
+            const errorMessage: string = 'Error Message';
+            sinon.stub(AuthService, 'getToken').rejects(new Error(errorMessage));
+
+            const req = { };
+            const res = { render: sinon.spy(), locals: {errormessage: ' '}};
+
+            await AuthController.postLoginForm(req as any, res as any);
+
+            expect(res.render.calledOnce).to.be.true;
+            expect(res.render.calledWith('loginForm.njk')).to.be.true;
+            expect(res.locals.errormessage).to.equal(errorMessage);
+        })
+    }),
+    describe('getLogOut', function () {
+        it('should logout successfully when no error thrown', async () => {
+
+            const res = { render: sinon.spy(), redirect: sinon.spy()};
+            const req = {session: {destroy: sinon.stub().callsFake(res.redirect('/'))}};
+
+            await AuthController.getLogout(req as any, res as any);
+
+            expect(res.redirect.calledOnce).to.be.true;
+            expect(res.redirect.calledWith('/')).to.be.true;
+        })
+    }),
+    describe('getLogOut', function () {
+        it('should render login page with error message when logout throws error', async () => {
+            const errorMessage: string = 'Error Message';
+            const res = { render: sinon.spy(), locals: {errormessage: ' '}};
+            const req = {session: {destroy: sinon.stub().callsFake(function errorLogout() {
+                res.locals.errormessage = errorMessage;
+                res.render('/login');
+            })
+        }};
+
+            await AuthController.getLogout(req as any, res as any);
+            expect(res.render.calledOnce).to.be.true;
+            expect(res.render.calledWith('/login')).to.be.true;
+            expect(res.locals.errormessage).to.equal(errorMessage);
+        })
+    })
+})
+
