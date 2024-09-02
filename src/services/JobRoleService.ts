459159abd4ab2dbf4a8bb82288214c3ccea52e38
mohammed <mohammedadnan.hoq@kainos.com>
2024-09-02 10:53:43 +0100

    Fixed URL for deployment

--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -1,14 +1,14 @@
-// import config from "../config";
+import config from "../config";
 import axios, { AxiosResponse } from "axios";
 import { JobRoleResponse } from "../models/JobRoleResponse";
 
 export const JOBROLEURL = "/job-roles/";
-// const URL = config.API_URL + JOBROLEURL;
+const URL = config.API_URL + JOBROLEURL;
 
 
 export const getJobRoles = async (): Promise<JobRoleResponse[]> => {
     try{
-        const response: AxiosResponse<JobRoleResponse[]> = await axios.get("http://localhost:8080/api/job-roles");
+        const response: AxiosResponse<JobRoleResponse[]> = await axios.get(URL);
         return response.data;
     } catch (e) {
         console.log(e);
