46de195d9bb212fda95cabc26fe0371eeeb3a230
Sam Dagnall <sam.dagnall@kainos.com>
2024-09-02 17:11:32 +0100

    Changed unauthenticatedRouter to lowercase

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import session from "express-session";
 import { getAllDatabases } from "./controllers/TestController";
 import { getAllJobRoles } from "./controllers/JobRoleController";
 import { dateFilter } from "./filters/DateFilter";
-import { unauthenticatedRouter } from "./routes/UnauthenticatedRouter";
+import { unauthenticatedRouter } from "./routes/unauthenticatedRouter";
 import { userRouter } from "./routes/UserRouter";
 const app = express();
 
