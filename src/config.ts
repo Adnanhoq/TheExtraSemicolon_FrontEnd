0c083c328b327558ac3f3c570f06558857c55d1f
jemorak <jemima.orakwue@hotmail.co.uk>
2024-09-03 09:56:02 +0100

    aws config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,16 @@
-
-export interface Config {
-    API_URL: string
+export type Config = {
+    API_URL: string,
+    AWS_ACCESS_KEY_ID: string,
+    AWS_SECRET_ACCESS_KEY: string,
+    BUCKET_NAME: string
    }
    
    export const config: Config  = {
-       API_URL: process.env.API_URL ?? '',
+       API_URL: process.env.API_URL,
+       AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+       AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+       BUCKET_NAME: process.env.S3_BUCKET
    };
    
    export default config
+
