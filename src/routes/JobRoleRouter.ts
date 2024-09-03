6cbb1ec60336c36d7bd0b0e852f536aa8ccbbad5
mohammed <mohammedadnan.hoq@kainos.com>
2024-09-03 14:13:05 +0100

    Added mongoose

--- src/routes/JobRoleRouter.ts
+++ src/routes/JobRoleRouter.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from 'express';
 import { Document } from 'mongoose';
+import { JobRoleResponse } from '../models/JobRoleResponse';
 
 const JobRoleRouter = express.Router();
 
 interface PaginatedJobRoles {
-  products: Document[];
+  jobRoles: JobRoleResponse[];
   total: number;
   limit: number;
   page: number;
   pages: number;
-  totalPublished: number;
+  totalJobRoles: number;
 }
 
-JobRoleRouter.get("/job-roles", async (req: Request, res: Response) => {});
+JobRoleRouter.get("/job-roles", async (req: Request, res: Response) => {
+    try{
+        const { roleId, roleName, locations, capability, band, closingDate, formattedLocations, page = 1, limit = 10 } = req.query;
+
+        const options = { skip: (parseInt(page as string, 10) - 1) * 
+            parseInt(limit as string, 10),
+               limit: parseInt(limit as string, 10)
+             };
+
+        const jobRoles = await JobRoleResponse.find(query, null, 
+            options).populate("vendor", "userName");
+    }
+});
