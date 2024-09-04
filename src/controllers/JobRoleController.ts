032607b55670d2036b7a7411f7bb42b4710dbc73
Sam Dagnall <sam.dagnall@kainos.com>
2024-09-04 12:15:17 +0100

    Removed passing in res.locals.role into each Controller render method

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -5,11 +5,11 @@ export const getAllJobRoles = async (req: express.Request, res: express.Response
 
     try{
         const jobRole = await getJobRoles(req.session.token ?? '');
-        res.render('jobRoles.njk', {jobRole: jobRole, token: req.session.token, role: res.locals.role as string});
+        res.render('jobRoles.njk', {jobRole: jobRole, token: req.session.token});
 
     } catch (e) {
         res.locals.errormessage = (e as Error).message;
-        res.render('errorPage.njk', {error: e as Error, token: req.session.token, role: res.locals.role as string});
+        res.render('errorPage.njk', {error: e as Error, token: req.session.token});
     }
     
 }
