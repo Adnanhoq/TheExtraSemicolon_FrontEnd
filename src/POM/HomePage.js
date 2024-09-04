a08c876476a560ca26feea18ab620f402b713b77
sunilsehmar78 <sunil.sehmar@kainos.com>
2024-09-04 10:04:45 +0100

    added localhost url for testing

--- src/POM/HomePage.js
+++ src/POM/HomePage.js
@@ -7,12 +7,16 @@ class HomePage {
     }
 
     async mainPage(){
-        this.driver.get('https://fqq3vg4qpt.eu-west-1.awsapprunner.com/')
+        //this.driver.get('https://fqq3vg4qpt.eu-west-1.awsapprunner.com/')
+        this.driver.get('http://localhost:3000/')
+
     }
 
 
     async openLoginPage(){
-        this.driver.get('https://fqq3vg4qpt.eu-west-1.awsapprunner.com/login')
+        //this.driver.get('https://fqq3vg4qpt.eu-west-1.awsapprunner.com/login')
+        this.driver.get('http://localhost:3000/login')
+
     }
     
     async logoutLink(){
