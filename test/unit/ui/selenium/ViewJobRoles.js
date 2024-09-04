acf793269b1d436853b5b357dbded65427b2aef7
sunilsehmar78 <sunil.sehmar@kainos.com>
2024-09-04 11:30:47 +0100

    added code to view job test

---
+++ test/unit/ui/selenium/ViewJobRoles.js
@@ -0,0 +1,19 @@
+const HomePage = require("../../../../src/POM/HomePage");
+
+async function ViewJobRoles() {
+
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        
+        const homepage = new HomePage(driver);
+
+        await homepage.mainPage();
+        await sleep(2000);
+    
+    } catch (error){
+        console.log('error: ', error);
+    } finally {
+        await driver.quit()
+    }
+}
