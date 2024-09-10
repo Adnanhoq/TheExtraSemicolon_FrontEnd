9824bde00de33d403f202a2bff7eb96e1596957b
sunilsehmar78 <sunil.sehmar@kainos.com>
2024-09-10 15:45:32 +0100

    modified test files, updated packages, updated scripts and readme

---
+++ test/ui/POM/HomePage.ts
@@ -0,0 +1,32 @@
+import { WebDriver, By, WebElement } from 'selenium-webdriver';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+class HomePage {
+    private driver: WebDriver;
+
+    constructor(driver: WebDriver) {
+        this.driver = driver;
+    }
+
+    async mainPage(): Promise<void> {
+        await this.driver.get(process.env.HOMEPAGE_URL_LOCAL as string);
+    }
+
+
+    async openLoginPage(): Promise<void> {
+        await this.driver.get(`${process.env.HOMEPAGE_URL_LOCAL as string}login`);
+    }
+
+
+    async jobRolePage(): Promise<void> {
+        await this.driver.get(`${process.env.HOMEPAGE_URL_LOCAL as string}job-roles`);
+    }
+
+    async logoutLink(): Promise<WebElement> {
+        return await this.driver.findElement(By.partialLinkText('Log Out'));
+    }
+}
+
+export default HomePage;
