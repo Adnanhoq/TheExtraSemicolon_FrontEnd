652649ab6623f248e090f924ceb77781d1f33905
AZZohaib <zohaib.ali@kainos.com>
2024-09-11 10:30:21 +0100

    removed unnecessary console.log

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,6 @@ app.use((req, res, next) => {
       req.session.theme = process.env.THEME ?? 'default';
   }  
   res.locals.theme = req.session.theme;
-  console.log(res.locals.theme);
   next();
 });
 
